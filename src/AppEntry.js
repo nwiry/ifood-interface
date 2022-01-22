725a20146722086d39a3c53786cb749d6ed2fcf7
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-01-22 17:44:02 -0300

    Home page e rotas adicionados

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,5 +1,5 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
-import App from './App';
+import Routes from './routes';
 
-registerRootComponent(App);
+registerRootComponent(Routes);
