fcc9194db6f35241dd32476f57da4b8c7a676f18
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-01-29 17:30:15 -0300

    Pagina principal finalizada

--- src/pages/Principal/index.js
+++ src/pages/Principal/index.js
@@ -14,14 +14,23 @@ import {
 } from './style';
 import { Text, Alert, ActivityIndicator } from 'react-native';
 
-export default function Principal({ navigation }) {
+import CategoriaItem from '../../components/CategoriaItem'
+import BannerItem from '../../components/BannerItem'
+import RestauranteItem from '../../components/RestauranteItem'
 
-  const [banners, setBanner] = useState();
-  const [categorias, setCategorias] = useState();
-  const [restaurantes, setRestaurantes] = useState();
-  const [loaded, setLoaded] = useState(false);
+
+export default function Principal() {
+
+
+  const [banners, setBanners] = useState([])
+  const [categorias, setCategorias] = useState([])
+  const [restaurantes, setRestaurantes] = useState([])
+  const [loaded, setLoaded] = useState(false)
+  const [tipo, setTipo] = useState('Entrega');
+
 
   useEffect(() => {
+
 
     async function buscaDados() {
 
@@ -31,41 +40,84 @@ export default function Principal({ navigation }) {
 
         const data = await response.json();
 
-        setLoaded(true);
-        setBanner(data.banner_principal);
+
+        setLoaded(true)
+
+
+        setBanners(data.banner_principal);
         setCategorias(data.categorias);
         setRestaurantes(data.restaurantes);
 
+
       } catch (e) {
-        Alert.alert('Erro ao consultar: ' + e);
+
+        Alert.alert('Erro consultada' + e);
+
       }
 
     }
 
-    buscaDados();
+    buscaDados()
 
-  });
+
+  }, [])
+
+
 
   const ViewHome = (props) => {
     return (
-      <Text>Principal</Text>
-    );
+      <ViewPrincipal>
+        <SelectTipo>
+          <ButtonTipoSelect onPress={() => setTipo('Entrega')}><TextTipoSelect selected={tipo == 'Entrega'}>Entrega</TextTipoSelect></ButtonTipoSelect>
+          <ButtonTipoSelect onPress={() => setTipo('Retirada')}><TextTipoSelect selected={tipo == 'Retirada'}>Retirada</TextTipoSelect></ButtonTipoSelect>
+        </SelectTipo>
+        <CategoriaView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {categorias.map(categoria => (
+            <CategoriaItem key={categoria.id} foto={categoria.img_url} texto={categoria.nome} />
+          ))}
+        </CategoriaView>
+        <BannerView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {banners.map(banner => (
+            <BannerItem key={banner.id} foto={banner.banner_img_url} />
+          ))}
+        </BannerView>
+        <TituloRestaurantes>Restaurantes</TituloRestaurantes>
+        <ViewRestaurantes>
+          {restaurantes.map(restaurante => (
+            <>
+              <RestauranteItem
+                key={restaurante.id}
+                foto={restaurante.url_img}
+                nome={restaurante.nome}
+                nota={restaurante.nota}
+                categoria={restaurante.categoria}
+                distancia={restaurante.distancia}
+                valorFrete={restaurante.valor_frete}
+                tempoEntrega={restaurante.tempo_entrega}
+              />
+            </>
+          ))}
+        </ViewRestaurantes>
+      </ViewPrincipal>
+    )
   }
 
   return (
     <>
       <StatusBar style="theme-dark" />
       <SafeAreaView>
-        {
-          loaded ? (
+        {loaded
+          ?
+          (
             <ViewHome />
-          ) : (
+          )
+          :
+          (
             <ViewActivity >
               <ActivityIndicator color="#F0001A" size="large" />
               <Text>Carregando dados aguarde...{loaded}</Text>
             </ViewActivity>
-          )
-        }
+          )}
       </SafeAreaView>
     </>
   );
