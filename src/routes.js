862b0073862dc969aed4ecc6e69b9b886495143f
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-01-29 14:30:40 -0300

    Main add

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,20 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./pages/Home";
+import Main from "./pages/Main";
 
 const Stack = createStackNavigator();
+
+const screenOptions = {
+    headerShown: false
+}
 
 const Routes = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator initialRouteName="Main" screenOptions={screenOptions} >
                 <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Main" component={Main} />
             </Stack.Navigator>
         </NavigationContainer>
     )
